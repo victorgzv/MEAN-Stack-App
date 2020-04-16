243e2dc2d443ec2c96d53a0d09a5bc065cf8fb66
Victor Gonzalez <victorgonvilla@gmail.com>
2020-04-16 22:05:42 +0100

    adding post api route

--- mean-stack-app/backend/app.js
+++ mean-stack-app/backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middlere");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from Express!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fdgad2",
+      title: "First dummy server-side title",
+      content: "some content coming from the server",
+    },
+    {
+      id: "fefe2",
+      title: "Second dummy server-side title",
+      content: "some content",
+    },
+  ];
+  res.status(200).json({
+    message: "Post fetched successfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
