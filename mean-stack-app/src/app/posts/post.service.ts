821006cd8fecb55cd8cb9637f321f388f7c5e7d5
Victor Gonzalez <victorgonvilla@gmail.com>
2020-04-18 18:37:23 +0100

    Adding Posts with an ID

--- mean-stack-app/src/app/posts/post.service.ts
+++ mean-stack-app/src/app/posts/post.service.ts
@@ -38,9 +38,11 @@ export class PostService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         console.log(responseData.message);
+        const id = responseData.postId;
+        post.id = id;
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
       });
