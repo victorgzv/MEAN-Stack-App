cbfff455a318108861ccb695ad9ff4f174089587
Victor Gonzalez <victorgonvilla@gmail.com>
2020-04-18 19:04:11 +0100

    added routing module and styles

--- mean-stack-app/src/app/app.module.ts
+++ mean-stack-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
