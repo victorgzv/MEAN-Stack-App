658b22fcd41c1a467e3dc3e76f19cd84eab126a8
victorgzv <victorgonvilla@gmail.com>
2020-04-19 12:12:53 +0100

    fixed create post and edit post reloding

--- mean-stack-app/src/app/posts/post.service.ts
+++ mean-stack-app/src/app/posts/post.service.ts
@@ -33,7 +33,10 @@ export class PostService {
   }
 
   getPost(id: string) {
-    return { ...this.posts.find((p) => p.id === id) };
+    //return { ...this.posts.find((p) => p.id === id) };
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/posts/' + id
+    );
   }
   getPostUdatedListener() {
     return this.postUpdated.asObservable();
@@ -68,12 +71,13 @@ export class PostService {
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
     this.http
-      .put<{ message: string }>(
-        'http://localhost:3000/api/posts/' + id,
-        post
-      )
-      .subscribe((responseData) => {
-        console.log(responseData.message);
+      .put<{ message: string }>('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postUpdated.next([...this.posts]);
       });
   }
 }
