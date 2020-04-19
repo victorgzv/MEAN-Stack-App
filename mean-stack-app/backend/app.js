3c4cfef6079c4c90f96843afff7225457acd0b47
victorgzv <victorgonvilla@gmail.com>
2020-04-19 22:11:15 +0100

    Fetching Images on the Frontend

--- mean-stack-app/backend/app.js
+++ mean-stack-app/backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const moongose = require("mongoose");
 const postsRoutes = require("./routes/posts");
@@ -19,6 +20,7 @@ moongose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
