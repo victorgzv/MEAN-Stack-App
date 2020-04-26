2840ba4510cb57061607c3f4ff6fae53741b5811
victorgzv <victorgonvilla@gmail.com>
2020-04-26 01:35:24 +0100

    creating a new user upon request

--- mean-stack-app/backend/routes/user.js
+++ mean-stack-app/backend/routes/user.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
-
-router.post("/signup"),(req, res, next)=>{
-
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const User = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    User.save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User created successfully",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      });
+  });
 });
 
 module.exports = router;
