6906046262e0d0dcfcdd647c3fce2dabf2ba284a
victorgzv <victorgonvilla@gmail.com>
2020-04-26 22:23:33 +0100

    implementing login with JWT

--- mean-stack-app/backend/routes/user.js
+++ mean-stack-app/backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
@@ -10,7 +11,8 @@ router.post("/signup", (req, res, next) => {
       email: req.body.email,
       password: hash,
     });
-    user.save()
+    user
+      .save()
       .then((result) => {
         res.status(201).json({
           message: "User created successfully",
@@ -25,4 +27,33 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (result) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      //create JWT
+      const token = jwt.sign(
+        { email: user.email, userId: user._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+    })
+    .catch((err) => {
+      return res.status(401).json({
+        message: "Auth failed",
+      });
+    });
+});
 module.exports = router;
